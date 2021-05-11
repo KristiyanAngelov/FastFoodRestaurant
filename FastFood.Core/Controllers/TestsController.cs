73754e833d0e5439c4478d37ac7955f0ebaa6e9a
Kristiyan Angelov <62059716+KristiyanAngelov@users.noreply.github.com>
2021-05-11 18:37:11 +0300

    Add test page functionality

---
+++ FastFood.Core/Controllers/TestsController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace FastFood.Core.Controllers
+{
+    public class TestsController : Controller
+    {
+        
+        public IActionResult Test()
+        {
+            return this.View();
+        }
+    }
+}
